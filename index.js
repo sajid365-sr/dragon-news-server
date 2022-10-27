6935e20bffd30d3068993f6e70a331129b99c378
sajid365-sr <sajidsorker86@gmail.com>
2022-10-28 02:33:10 +0600

    news search by id and category setup

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-const categories = require('./data/categories.json');
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
-app.get('/', (req,res) =>{
-    res.send('News API Running');
+app.get("/", (req, res) => {
+  res.send("News API Running");
 });
 
-app.get('/news-categories', (req,res) =>{
-    res.send(categories);
-})
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+
+// Get specific category news
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+// Get specific id news
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 
 app.listen(port, () => {
-    console.log('Dragon News Server Running on Port: ', port);
-})
+  console.log("Dragon News Server Running on Port: ", port);
+});
